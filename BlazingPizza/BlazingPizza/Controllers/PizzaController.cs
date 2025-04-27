d01fc244a19778158d698b3f65216a382d7180f5
Paiva <danpaiva@live.com>
2025-04-27 13:55:42 -0300

    feat: add controller

---
+++ BlazingPizza/BlazingPizza/Controllers/PizzaController.cs
@@ -0,0 +1,28 @@
+﻿using BlazingPizza.Data;
+using BlazingPizza.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazingPizza.Controllers;
+
+public class PizzaController
+{
+
+    [Route("pizzas")]
+    [ApiController]
+    public class SpecialsController : Controller
+    {
+        private readonly PizzaStoreContext _db;
+
+        public SpecialsController(PizzaStoreContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<PizzaSpecial>>> GetPizzas()
+        {
+            return (await _db.Tb_Pizzas.ToListAsync()).OrderByDescending(s => s.PrecoBase).ToList();
+        }
+    }
+}
