9f05d123710185d39adb01fd98aaa2e9d3f377c4
Paiva <danpaiva@live.com>
2025-04-27 16:52:01 -0300

    feat: add classe de Estado da Pizza

---
+++ BlazingPizza/BlazingPizza/Services/PedidoEstado.cs
@@ -0,0 +1,38 @@
+﻿using BlazingPizza.Model;
+
+namespace BlazingPizza.Services;
+
+public class PedidoEstado
+{
+    public bool MostrarJanelaConfig { get; private set; }
+    public Pizza ConfiguracaoPizza { get; private set; }
+    public Order Pedido { get; private set; } = new Order();
+
+    public void MostrarJanelaConfiguracaoPizza(PizzaSpecial special)
+    {
+        ConfiguracaoPizza = new Pizza()
+        {
+            PizzaSpecial = special,
+            SpecialId = special.Id,
+            Tamanho = Pizza.DefaultSize,
+            ListaRecheios = new List<PizzaTopping>(),
+        };
+
+        MostrarJanelaConfig = true;
+    }
+
+    public void CancelarJanelaConfiguracaoPizza()
+    {
+        ConfiguracaoPizza = null;
+
+        MostrarJanelaConfig = false;
+    }
+
+    public void ConfirmarJanelaConfiguracaoPizza()
+    {
+        Pedido.ListaPizzas.Add(ConfiguracaoPizza);
+        ConfiguracaoPizza = null;
+
+        MostrarJanelaConfig = false;
+    }
+}
